cb954db29d0dce413bb9e8347bb93d782fab3a83
Wade Peterson <wade.peterson@ca.com>
2017-06-03 21:49:22 -0600

    Revert reducer to simpler form

--- src/reducer.ts
+++ src/reducer.ts
@@ -7,38 +7,22 @@ const initialState = {};
 
 export type Action<T> = { type: string, payload: T };
 
-function updateDeep(currentNodeValue: {[key: string]: any} = {}, currentPath: string[], targetPath: string[], targetValue: any) {
-  let newValueForCurrentNode: any;
-
-  if (targetPath.length === 0) {
-    if (isObject(targetValue)) {
-      newValueForCurrentNode = Object.keys(targetValue).reduce((acc, key) => {
-        return updateDeep(acc, [], [key], targetValue[key]);
-      }, currentNodeValue);
-    } else {
-      newValueForCurrentNode = targetValue;
-    }
-  } else {
-    const key = targetPath[0];
-    const childNodeValue = updateDeep(currentNodeValue[key], currentPath.concat(key), targetPath.slice(1), targetValue);
-    newValueForCurrentNode = { ...currentNodeValue, [key]: childNodeValue };
-
-    // If I only have one child, and its value is null, then I should also be null
-    if (childNodeValue === null) {
-      if (Object.keys(newValueForCurrentNode).length === 1) {
-        newValueForCurrentNode = null;
-      } else {
-        delete newValueForCurrentNode[key];
-      }
-    }
+function updateDeep(state: State = {}, path: string[], value: any): any {
+  if (path.length === 0) {
+    return value;
   }
 
-  return newValueForCurrentNode;
+  const key = path[0];
+
+  return {
+    ...state,
+    [key]: updateDeep(state[key], path.slice(1), value)
+  };
 }
 
 function handleSetNodeValue(state: State, action: Action<Actions.SetNodeValuePayload>) {
   const path = action.payload.path.split('/').filter(p => !!p);
-  return updateDeep(state, [], path, action.payload.value) || {};
+  return updateDeep(state, path, action.payload.value);
 }
 
 export default function (state = initialState, action: Action<any>) {
